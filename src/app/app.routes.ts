087f800528cdcd857f69303c20a5a538cd849c9f
PabloHoties <pablohotiesmarques@gmail.com>
2024-12-21 18:00:44 -0300

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
+import { AutenticarFuncionarioComponent } from './components/pages/autenticar-funcionario/autenticar-funcionario.component';
+import { CriarFuncionarioComponent } from './components/pages/criar-funcionario/criar-funcionario.component';
+import { RelatorioGestorComponent } from './components/pages/relatorio-gestor/relatorio-gestor.component';
+import { MarcarPontoComponent } from './components/pages/marcar-ponto/marcar-ponto.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'pages/funcionarios/autenticar',
+        component: AutenticarFuncionarioComponent
+    },
+    {
+        path: 'pages/relatorio-gestor',
+        component: RelatorioGestorComponent
+    },
+    {
+        path:'pages/marcar-ponto',
+        component: MarcarPontoComponent
+    },
+    {
+        path: '', pathMatch: 'full',
+        redirectTo: 'pages/funcionarios/autenticar'
+    }
+];
